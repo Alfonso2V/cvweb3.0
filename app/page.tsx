ece8710f3443a3383f9128158f34bb35d90b2541
AlfonsoV <ravgamerica@hotmail.com>
2024-10-19 11:48:52 -0600

    feat: format files and set intial values pages names

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export const runtime = "edge";
 
 export default function Home() {
   const fullYear = new Date().getFullYear();
- return (
-  <section className="w-full h-screen flex justify-center items-center">
-    <h1>CVWEb3.0 Alfonso Verastegui ©{fullYear}</h1>
-  </section>
- );
+  return (
+    <section className="w-full h-screen flex justify-center items-center">
+      <h1>CVWEb3.0 Alfonso Verastegui ©{fullYear}</h1>
+    </section>
+  );
 }
